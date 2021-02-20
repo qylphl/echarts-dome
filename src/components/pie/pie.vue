e1adcec77124d0740172653f989a13ecd6f6cea8
qylphl <qylpgh@163.com>
2021-02-20 09:18:14 +0800

    优化

--- src/components/pie/pie.vue
+++ src/components/pie/pie.vue
@@ -48,13 +48,13 @@ export default {
       });
       pieChart.setOption(option, true);
       if(this.isCheck == 1){ // 设置默认选中高亮
-        that.tipTitle = '注："boxTitle"和"boxWidth"不是echart中option所需要的属性，可以删掉。如果需要默认选中高量的话，需要引入utils/chartsClass.js文件，使用setHighlight方法进行配置。',
+        that.tipTitle = '注："boxTitle"和"boxWidth"不是echart中option所需要的属性，可以删掉。如果需要默认选中高量的话，则要拷贝utils/chartsClass.js文件，使用setHighlight方法进行配置。',
         setHighlight(
           pieChart,
           that.data.map((v) => v.value)
         );
       }else if(this.isCheck == 2){  // 带虚线边框饼图的默认高亮
-        that.tipTitle = '注："boxTitle"和"boxWidth"不是echart中option所需要的属性，可以删掉。如果需要默认选中高量的话，需要引入utils/chartsClass.js文件，使用setBorderPieHighlight方法进行配置。',
+        that.tipTitle = '注："boxTitle"和"boxWidth"不是echart中option所需要的属性，可以删掉。如果需要默认选中高量的话，则要拷贝utils/chartsClass.js文件，使用setBorderPieHighlight方法进行配置。',
         setBorderPieHighlight(
           pieChart,
           that.data.map((v) => v.value)
