f65b307599a67ef340e3db61b3ee48c8a81c203b
qylphl <qylpgh@163.com>
2021-03-05 17:18:24 +0800

    add

--- src/page/animationNav.vue
+++ src/page/animationNav.vue
@@ -65,7 +65,7 @@ export default {
       // 右侧导航菜单
       rightList: [
         { title: "导航相关动画", num: "4" },
-        { title: "菜单相关动画", num: "3" },
+        { title: "菜单相关动画", num: "4" },
       ],
       scroll: "",
       chooseIndex: 0, // 选中右侧导航的index值
